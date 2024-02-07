0b9bf41c3cb36867ffd16508e268d49397b11f48
botechiadev <botechiadev@gmail.com>
2024-02-07 06:26:59 -0300

    4-deploy

--- src/components/CardBox1/styled.CardBox.js
+++ src/components/CardBox1/styled.CardBox.js
@@ -1,13 +1,20 @@
-import styled from 'styled-components'
-import bg from '../../assets/img/bg.png'
+import styled from 'styled-components';
+import bg from '../../assets/img/bg.png';
+
 export const ContainerCardBox = styled.li`
-    background: url(bg);
-    border: 3px solid #070d45;
-    width: 32%;
-    padding: 1rem;
+  background-image: url(${bg});
+  background-size: cover;
+  background-position: center;
+  border: 3px solid #070d45;
+  width: 32%;
+  padding: 1rem;
+  transition: box-shadow 0.3s ease;
 
+  @media screen and (max-width: 720px) {
+    width: 80%;
+  }
 
-    @media screen and (max-width: 720px){
-        width: 80%
-    }
-`
+  &:hover {
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+  }
+`;
